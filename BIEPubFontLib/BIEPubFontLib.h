5a8fce5383b5678ee79b52626b2538c9496d44de
Bogdan Iusco <bogdan.iusco@gmail.com>
2014-06-17 18:04:35 +0300

    Create lib target.

---
+++ BIEPubFontLib/BIEPubFontLib.h
@@ -0,0 +1,13 @@
+//
+//  BIEPubFontLib.h
+//  BIEPubFontLib
+//
+//  Created by Bogdan Iusco on 6/17/14.
+//  Copyright (c) 2014 Grigaci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BIEPubFontLib : NSObject
+
+@end
---
+++ BIEPubFontLib/BIEPubFontLib.m
@@ -0,0 +1,13 @@
+//
+//  BIEPubFontLib.m
+//  BIEPubFontLib
+//
+//  Created by Bogdan Iusco on 6/17/14.
+//  Copyright (c) 2014 Grigaci. All rights reserved.
+//
+
+#import "BIEPubFontLib.h"
+
+@implementation BIEPubFontLib
+
+@end
